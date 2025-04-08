6a6fdcf85585c7fbd1906969b5cb0a866b36e1e8
Daniel-Salvador <daniovictor@hotmail.com>
2025-04-08 15:42:26 -0300

    REFACTOR: files organization

---
+++ run_simulation.py
@@ -0,0 +1,19 @@
+import mujoco
+import mujoco.viewer
+import tyro
+from dataclasses import dataclass
+
+
+@dataclass
+class Args:
+    model_path: str = "mujoco_study/terrain_test.xml"
+
+
+args = tyro.cli(Args)
+mujoco_model = mujoco.MjModel.from_xml_path(args.model_path)
+
+data = mujoco.MjData(mujoco_model)
+with mujoco.viewer.launch(mujoco_model) as viewer:
+    while viewer.is_running():
+        mujoco.mj_step(mujoco_model, data)
+        viewer.sync()
